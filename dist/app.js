7fee333a825c4205ff810c39d44d98e28d7d4a0c
thomas-ode <thomas.ode.w@gmail.com>
2021-10-28 16:00:34 +0200

    init repos back

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// Start Server
+app.listen(4000, () => console.log("Server started on 4000"));
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const app = express();
+
+// Start Server
+app.listen(4000, () => console.log('Server started on 4000'));
