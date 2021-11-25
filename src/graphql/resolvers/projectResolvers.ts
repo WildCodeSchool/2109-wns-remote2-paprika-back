9e85137699a573373205f5e403ba55b9baa1b5d8
lolarne <rne.lola@gmail.com>
2021-11-25 17:20:15 +0100

    all queries and mutations work

--- src/graphql/resolvers/projectResolvers.ts
+++ src/graphql/resolvers/projectResolvers.ts
@@ -8,16 +8,16 @@ const dateScalar = new GraphQLScalarType({
   name: 'Date',
   description: 'Date custom scalar type',
   serialize(value) {
-    return value.getTime(); // Convert outgoing Date to integer for JSON
+    return value.getTime();
   },
   parseValue(value) {
-    return new Date(value); // Convert incoming integer to Date
+    return new Date(value);
   },
   parseLiteral(ast) {
     if (ast.kind === Kind.INT) {
-      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+      return new Date(parseInt(ast.value, 10));
     }
-    return null; // Invalid hard-coded value (not an integer)
+    return null;
   }
 });
 
@@ -28,7 +28,7 @@ export default {
       const projects = await prisma.project.findMany();
       return projects;
     },
-    getTask: async ({ projectId }: { projectId: string }) => {
+    getProject: async (_: any, { projectId }: { projectId: string }) => {
       const project = await prisma.project.findUnique({
         where: {
           id: projectId
@@ -39,39 +39,46 @@ export default {
   },
 
   Mutation: {
-    createProject: async ({ input }: { input: ProjectInput }) => {
+    createProject: async (
+      _: any,
+      { projectInput }: { projectInput: ProjectInput }
+    ) => {
       return await prisma.project.create({
         data: {
-          name: input.name,
-          client: input.client,
-          description: input.description,
+          name: projectInput.name,
+          client: projectInput.client,
+          description: projectInput.description
         }
       });
     },
-    deleteProject: async ({ projectId }: { projectId: string }) => {
-      return await prisma.project.delete({
+    deleteProject: async (_: any, { projectId }: { projectId: string }) => {
+      const deleted = await prisma.project.delete({
         where: {
           id: projectId
         }
       });
+      return !!deleted;
     },
-    updateProject: async ({
-      projectId,
-      input
-    }: {
-      projectId: string;
-      input: UpdateProjectInput;
-    }) => {
+    updateProject: async (
+      _: any,
+      {
+        projectId,
+        updateProjectInput
+      }: {
+        projectId: string;
+        updateProjectInput: UpdateProjectInput;
+      }
+    ) => {
       return await prisma.project.update({
         where: {
           id: projectId
         },
         data: {
-          startAt: input.startAt,
-          endAt: input.endAt,
-          name: input.name  || undefined,
-          client: input.client  || undefined,
-          description: input.description  || undefined,
+          startAt: updateProjectInput.startAt,
+          endAt: updateProjectInput.endAt,
+          name: updateProjectInput.name || undefined,
+          client: updateProjectInput.client || undefined,
+          description: updateProjectInput.description || undefined
         }
       });
     }
